47c0524ddee7eb0b91a39d173ef6d76651f9f373
ManasaChatakondu <manasa.cse28@gmail.com>
2017-03-21 21:04:15 +1100

    Test

---
+++ MovieWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using MovieWebApplication.Models;
+
+namespace MovieWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        string cinemaurl = "/api/cinemaworld/movies";
+        string filmurl = "/api/filmworld/movies";
+        
+        // GET: Home
+        public async Task<ActionResult> Index()
+        {
+            ViewBag.Movies = "";
+            MoviesList cinemaModel = null;
+            MoviesList filmModel = null;
+            List<MovieModel> model = new List<MovieModel>();
+            string jsonCinemadata = await getMoviesbyUrl(cinemaurl);
+            string jsonFilmdata = await getMoviesbyUrl(filmurl);
+
+            if (jsonCinemadata == "NoRecords" && jsonFilmdata == "NoRecords")
+            {
+                ViewBag.Movies = "No Movies";
+                return View();
+            }
+            if (jsonCinemadata != "NoRecords")
+            {
+                cinemaModel = JsonConvert.DeserializeObject<MoviesList>(jsonCinemadata);
+                model.AddRange(cinemaModel.Movies);
+            }
+            if (jsonFilmdata != "NoRecords")
+            {
+                filmModel = JsonConvert.DeserializeObject<MoviesList>(jsonFilmdata);
+                model.AddRange(filmModel.Movies);
+            }
+            return View(model);
+        }
+        private async Task<string> getMoviesbyUrl(string url)
+        {
+            var webApi = new MovieApiController();
+            return await webApi.getMovies(url);
+        }
+    }
+}
---
+++ MovieWebApplication/Controllers/MovieApiController.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MovieWebApplication.Controllers
+{
+    public class MovieApiController : ApiController
+    {
+        public async Task<string> getMovies(string url)
+        {
+            string apiBaseUri = "http://webjetapitest.azurewebsites.net";
+            string token = ConfigurationManager.AppSettings["Token"];
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(apiBaseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("x-access-token", token);
+
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = await response.Content.ReadAsStringAsync();
+                    return data;
+                    
+                }
+                else
+                    return "NoRecords";
+            }
+
+        }
+    }
+}
